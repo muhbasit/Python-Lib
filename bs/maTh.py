f840e5ec3b59fea4da24e40a4eb4f7ed02e6e367
Muhammad Basit <139772050+muhbasit@users.noreply.github.com>
2024-09-24 00:06:01 +0400

    Update maTh.py

--- bs/maTh.py
+++ bs/maTh.py
@@ -212,3 +212,162 @@ class maTh:
         if is_list:
             return [x ** (1/3) for x in value]
         return value ** (1/3)
+
+    @staticmethod
+    def inc(value):
+        """Increment a number or each element in a list by 1."""
+        is_list = type(value) == list
+        if is_list:
+            return [x + 1 for x in value]
+        return value + 1
+
+    @staticmethod
+    def dec(value):
+        """Decrement a number or each element in a list by 1."""
+        is_list = type(value) == list
+        if is_list:
+            return [x - 1 for x in value]
+        return value - 1
+
+    @staticmethod
+    def assign(alpha, operation=None):
+        """Perform a selected assignment operation."""
+        
+        def add(value):
+            """Add 2 to the current value."""
+            return value + 2
+
+        def sub(value):
+            """Subtract 2 from the current value."""
+            return value - 2
+
+        def mul(value):
+            """Multiply the current value by 2."""
+            return value * 2
+
+        def div(value):
+            """Divide the current value by 2."""
+            return value / 2 if value != 0 else "Cannot divide by zero"
+
+        def mod(value):
+            """Get the modulus of the current value by 2."""
+            return value % 2
+
+        def exp(value):
+            """Calculate the exponent of the current value raised to 2."""
+            return value ** 2
+
+        def and_op(value):
+            """Perform bitwise AND with 1."""
+            return value & 1
+
+        def or_op(value):
+            """Perform bitwise OR with 1."""
+            return value | 1
+
+        def r_shift(value):
+            """Right shift the current value by 1."""
+            return value >> 1
+
+        def l_shift(value):
+            """Left shift the current value by 1."""
+            return value << 1
+
+        # Check if alpha is a list
+        is_list = type(alpha) == list
+        results = {}
+
+        if operation is None:  # Perform all operations
+            operations = [
+                'Add Assignment', 'Subtract Assignment', 'Multiply Assignment', 
+                'Divide Assignment', 'Modulus Assignment', 'Exponent Assignment', 
+                'Bitwise AND Assignment', 'Bitwise OR Assignment', 
+                'Right Shift Assignment', 'Left Shift Assignment'
+            ]
+            for op in operations:
+                if is_list:
+                    if op == 'Add Assignment':
+                        results[op] = [add(x) for x in alpha]
+                    elif op == 'Subtract Assignment':
+                        results[op] = [sub(x) for x in alpha]
+                    elif op == 'Multiply Assignment':
+                        results[op] = [mul(x) for x in alpha]
+                    elif op == 'Divide Assignment':
+                        results[op] = [div(x) for x in alpha]
+                    elif op == 'Modulus Assignment':
+                        results[op] = [mod(x) for x in alpha]
+                    elif op == 'Exponent Assignment':
+                        results[op] = [exp(x) for x in alpha]
+                    elif op == 'Bitwise AND Assignment':
+                        results[op] = [and_op(x) for x in alpha]
+                    elif op == 'Bitwise OR Assignment':
+                        results[op] = [or_op(x) for x in alpha]
+                    elif op == 'Right Shift Assignment':
+                        results[op] = [r_shift(x) for x in alpha]
+                    elif op == 'Left Shift Assignment':
+                        results[op] = [l_shift(x) for x in alpha]
+                else:
+                    if op == 'Add Assignment':
+                        results[op] = add(alpha)
+                    elif op == 'Subtract Assignment':
+                        results[op] = sub(alpha)
+                    elif op == 'Multiply Assignment':
+                        results[op] = mul(alpha)
+                    elif op == 'Divide Assignment':
+                        results[op] = div(alpha)
+                    elif op == 'Modulus Assignment':
+                        results[op] = mod(alpha)
+                    elif op == 'Exponent Assignment':
+                        results[op] = exp(alpha)
+                    elif op == 'Bitwise AND Assignment':
+                        results[op] = and_op(alpha)
+                    elif op == 'Bitwise OR Assignment':
+                        results[op] = or_op(alpha)
+                    elif op == 'Right Shift Assignment':
+                        results[op] = r_shift(alpha)
+                    elif op == 'Left Shift Assignment':
+                        results[op] = l_shift(alpha)
+            return results
+        else:  # Perform the specified operation
+            if is_list:
+                if operation == 'Add Assignment':
+                    return [add(x) for x in alpha]
+                elif operation == 'Subtract Assignment':
+                    return [sub(x) for x in alpha]
+                elif operation == 'Multiply Assignment':
+                    return [mul(x) for x in alpha]
+                elif operation == 'Divide Assignment':
+                    return [div(x) for x in alpha]
+                elif operation == 'Modulus Assignment':
+                    return [mod(x) for x in alpha]
+                elif operation == 'Exponent Assignment':
+                    return [exp(x) for x in alpha]
+                elif operation == 'Bitwise AND Assignment':
+                    return [and_op(x) for x in alpha]
+                elif operation == 'Bitwise OR Assignment':
+                    return [or_op(x) for x in alpha]
+                elif operation == 'Right Shift Assignment':
+                    return [r_shift(x) for x in alpha]
+                elif operation == 'Left Shift Assignment':
+                    return [l_shift(x) for x in alpha]
+            else:
+                if operation == 'Add Assignment':
+                    return add(alpha)
+                elif operation == 'Subtract Assignment':
+                    return sub(alpha)
+                elif operation == 'Multiply Assignment':
+                    return mul(alpha)
+                elif operation == 'Divide Assignment':
+                    return div(alpha)
+                elif operation == 'Modulus Assignment':
+                    return mod(alpha)
+                elif operation == 'Exponent Assignment':
+                    return exp(alpha)
+                elif operation == 'Bitwise AND Assignment':
+                    return and_op(alpha)
+                elif operation == 'Bitwise OR Assignment':
+                    return or_op(alpha)
+                elif operation == 'Right Shift Assignment':
+                    return r_shift(alpha)
+                elif operation == 'Left Shift Assignment':
+                    return l_shift(alpha)
